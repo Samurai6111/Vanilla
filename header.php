6708421426d660c815836961a99c4bba59d6d8b8
Samurai6111 <samurai.blue6111@gmail.com>
2023-01-27 20:20:39 +0900

    google font 読み込み 削除

--- header.php
+++ header.php
@@ -24,23 +24,9 @@
 	<link rel="shortcut icon"
 				href="img/common/favicon.png">
 
-
-	<!-- google font 読み込み -->
-	<link rel="preconnect"
-				href="https://fonts.googleapis.com">
-	<link rel="preconnect"
-				href="https://fonts.gstatic.com"
-				crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-				rel="stylesheet">
-
-
-
-
 	<!-- swiper 読み込み -->
 	<link rel="stylesheet"
 				href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
-
 	<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
 	<?php wp_head(); ?>
 </head>
