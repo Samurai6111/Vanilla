629a122b9b99713eeda168e1974aefd5f024d2ad
Samurai6111 <samurai.blue6111@gmail.com>
2023-03-24 13:14:34 +0900

    feat : ページネーションのスタイルリニューアル

--- Assets/Components/c-pagination.php
+++ Assets/Components/c-pagination.php
@@ -1,13 +1,35 @@
 <div class="pagination">
-	<?php echo paginate_links(array(
-				'format' => 'page/%#%/',
-				'current' => max(1, get_query_var('page')),
-				'total' => $WP_post->max_num_pages,
-				'type' => 'list',
-				'mid_size' => '1',
-				'prev_text' => '<i class="fas fa-angle-left"></i>',
-				'next_text' => '<i class="fas fa-angle-right"></i>',
-			));
-			wp_reset_postdata();
-			?>
+	<?php $pagination_array =  paginate_links(array(
+		// 'format' => 'page/%#%/',
+		'current' => max(1, vanilla_paged()),
+		'total' => $WP_post->max_num_pages,
+		'type' => 'array',
+		'mid_size' => '1',
+		'prev_text' => 'PREV',
+		'next_text' => 'NEXT',
+	));
+	wp_reset_postdata();
+
+	?>
+	<ul class="pagination__list">
+		<?php
+		//= 1ページ目の時にもPREVを表示 ====
+		if (vanilla_paged() === 1) { ?>
+			<li class="pagination__item">
+				<span class="prev page-numbers -disabled">PREV</span>
+			</li>
+		<?php } ?>
+
+		<?php foreach ($pagination_array as $pagination) { ?>
+			<li class="pagination__item"><?php echo $pagination ?></li>
+		<?php } ?>
+
+		<?php
+		//= 最後のページの時にもNEXTを表示 ====
+		if (vanilla_paged() === (int)$WP_post->max_num_pages) { ?>
+			<li class="pagination__item">
+				<span class="next page-numbers -disabled">NEXT</span>
+			</li>
+		<?php } ?>
+	</ul>
 </div>
