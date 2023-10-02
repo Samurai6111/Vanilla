063bc0f8df6fc90d41bb4362b90e17d6ff792bae
Samurai6111 <samurai.blue6111@gmail.com>
2023-10-02 19:10:33 +0900

    feat : Yoast SEOのOpen Graphメタタグを削除するjコード追加

--- App/Wordpress/App-wordpress.php
+++ App/Wordpress/App-wordpress.php
@@ -1,16 +1,15 @@
 <?php
+
 /**
-* 管理画面→「外観」→「メニュー」のナビメニューを追加
-*/
-function vanilla_register_nav_menus()
-{
+ * 管理画面→「外観」→「メニュー」のナビメニューを追加
+ */
+function vanilla_register_nav_menus() {
 	register_nav_menu('vanilla-nav-menu-pc', 'PCメニュー');
 	register_nav_menu('vanilla-nav-menu-sp', 'SPメニュー');
 }
 add_action('init', 'vanilla_register_nav_menus', 10);
 
-function vanilla_register_nav_menu($menu_slug, $menu_id)
-{
+function vanilla_register_nav_menu($menu_slug, $menu_id) {
 	if (!has_nav_menu($menu_slug)) {
 		$locations = get_theme_mod('nav_menu_locations');
 		$locations[$menu_slug] = $menu_id;
@@ -21,8 +20,7 @@ function vanilla_register_nav_menu($menu_slug, $menu_id)
 //--------------------------------------------------
 // ユーザーごとに管理者のサイドバーを変更
 //--------------------------------------------------
-function vanilla_hide_admin_menu()
-{
+function vanilla_hide_admin_menu() {
 	global $current_user;
 
 	if ($current_user->user_login === 'クライアントのユーザー名が入ります' && is_admin()) {
@@ -71,8 +69,7 @@ function vanilla_hide_admin_menu()
 /*--------------------------------------------------
 /* カスタム投稿「イベント」の管理画面の投稿一覧にカラムを増やす
 /*------------------------------------------------*/
-function vanilla_custom_event_posts_columns($columns)
-{
+function vanilla_custom_event_posts_columns($columns) {
 	unset($columns['date']);
 	unset($columns['category']);
 	return array_merge(
@@ -87,8 +84,7 @@ add_filter('manage_event_posts_columns', 'vanilla_custom_event_posts_columns');
 /*--------------------------------------------------
 /* カスタム投稿「イベント」の管理画面の投稿一覧のカラムに値を出力する
 /*------------------------------------------------*/
-function display_event_posts_custom_column($column, $post_id)
-{
+function display_event_posts_custom_column($column, $post_id) {
 	$post__type = get_post_type($post_id);
 	if ($post__type === 'event') {
 
@@ -107,21 +103,81 @@ function display_event_posts_custom_column($column, $post_id)
 add_action('manage_event_posts_custom_column', 'display_event_posts_custom_column', 10, 2);
 
 /**
-* 管理画面に独自のスタイルを挿入する
-*/
-function vanilla_custom_admin_style() {
-	?>
-	<style>
-		.acf-field.acf-field-checkbox .acf-checkbox-list {
-			display: flex;
-			flex-wrap: wrap;
+ * metaタグ設定
+ */
+function vanilla_meta_ogp() {
+	if (is_front_page() || is_home() || is_singular()) {
+
+		/*初期設定*/
+
+		// 画像 （アイキャッチ画像が無い時に使用する代替画像URL）
+		$ogp_image = get_template_directory_uri() . '/Img/SEO/OGP_1200x630.png';
+		// Twitterのアカウント名 (@xxx)
+		$twitter_site = '';
+		// Twitterカードの種類（summary_large_image または summary を指定）
+		$twitter_card = 'summary_large_image';
+		// Facebook APP ID
+		$facebook_app_id = '';
+
+		global $post;
+		$ogp_title = '';
+		$ogp_description = '';
+		$ogp_url = '';
+		$html = '';
+
+		//== 記事＆固定ページ ====
+		if (is_singular()) {
+			setup_postdata($post);
+			$ogp_title = $post->post_title;
+			$ogp_description = mb_substr(get_the_excerpt(), 0, 100);
+			$ogp_url = get_permalink();
+			wp_reset_postdata();
+		} elseif
+		//== トップページ ====
+		(is_front_page() || is_home()) {
+			$ogp_title = get_bloginfo('name');
+			$ogp_description = get_bloginfo('description');
+			$ogp_url = home_url();
 		}
 
-		.acf-field.acf-field-checkbox .acf-checkbox-list > li {
-			width: 33%;
-			margin: 0;
+		// og:type
+		$ogp_type = (is_front_page() || is_home()) ? 'website' : 'article';
+
+		// og:image
+		if (is_singular() && has_post_thumbnail()) {
+			$ps_thumb = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');
+			$ogp_image = $ps_thumb[0];
 		}
-  </style>
-	<?php
+
+		// 出力するOGPタグをまとめる
+		$html = "\n";
+		$html .= "<title>{$ogp_title} | {$ogp_description}</title> \n";
+		$html .= "<meta property='og:locale' content='ja_JP'> \n";
+		$html .= '<meta property="og:title" content="' . esc_attr($ogp_title) . '">' . "\n";
+		$html .= '<meta property="og:description" content="' . esc_attr($ogp_description) . '">' . "\n";
+		$html .= '<meta property="og:type" content="' . $ogp_type . '">' . "\n";
+		$html .= '<meta property="og:url" content="' . esc_url($ogp_url) . '">' . "\n";
+		$html .= '<meta property="og:image" content="' . esc_url($ogp_image) . '">' . "\n";
+		$html .= '<meta property="og:site_name" content="' . esc_attr(get_bloginfo('name')) . '">' . "\n";
+		$html .= '<meta name="twitter:card" content="' . $twitter_card . '">' . "\n";
+		$html .= '<meta name="twitter:site" content="' . $twitter_site . '">' . "\n";
+		$html .= '<meta property="og:locale" content="ja_JP">' . "\n";
+
+		if ($facebook_app_id != "") {
+			$html .= '<meta property="fb:app_id" content="' . $facebook_app_id . '">' . "\n";
+		}
+
+		echo $html;
+	}
 }
-add_action('admin_head', 'vanilla_custom_admin_style');
+// headタグ内にOGPを出力する
+add_action('wp_head', 'vanilla_meta_ogp');
+
+/**
+ * Yoast SEOのOpen Graphメタタグを削除
+ *
+ */
+function  remove_yoast_seo_meta_tags() {
+	remove_all_actions('wpseo_head');
+}
+add_action('wp_head', 'remove_yoast_seo_meta_tags', 1);
