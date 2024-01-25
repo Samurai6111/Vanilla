7edca3b74ce531df29525d319d5e4f74323d2d34
Samurai6111 <samurai.blue6111@gmail.com>
2024-01-25 14:39:14 +0900

    feat : プラポリのショートコード追加

--- App/Function/App-funtions.php
+++ App/Function/App-funtions.php
@@ -1,1 +1,8 @@
 <?php
+
+
+function privacy_policiy_link() {
+	$privacy_policiy_link = home_url('/privacypolicy/');
+	return $privacy_policiy_link;
+}
+add_shortcode('privacy_policiy_link', 'privacy_policiy_link');
